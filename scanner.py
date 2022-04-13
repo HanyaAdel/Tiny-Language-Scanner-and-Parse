8b53d982ac625ee3343d2b134e0d68ef268282c6
Mohamed Ayman <mohamed16130@stemegypt.edu.eg>
2022-04-13 23:52:47 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,114 +1,103 @@
-from PyQt5.QtWidgets import QApplication, QPushButton, QGridLayout, QLabel, QTextEdit, QWidget
+class Scanner:
 
-reserved_words = ('repeat', 'until')
+    def scan(self, input_text):
+        reserved_words = ('repeat', 'until')
 
-comparators = ('=', '>', '<', '>=', '<=')
+        comparison_operators = ('=', '>', '<', '>=', '<=')
+        comp = ('<', '>')
 
-assignment = (':=')
+        white_spaces = ('\n', '\t', ' ')  # consider adding \0
+        assignment = (':=')
 
-tokens = (';', '=')
+        special_tokens = (';', '=')
+        special_characters = ('<', '>', '=', ':', ";")
+        tokens = []
+        types = []
 
-lexemes = []
-types = []
+        i = 0
+        state = "start"
+        temp_token = ""
+        while i < len(input_text):
+            if input_text[i].isdigit() or input_text[i].isalpha() or input_text[i] in white_spaces or input_text[i] in special_characters:
+                if state == "start":
+                    if input_text[i] in white_spaces:
+                        state = "start"
+                    elif input_text[i].isalpha():
+                        temp_token += input_text[i]
+                        state = "inid"
+                    elif input_text[i].isdigit():
+                        temp_token += input_text[i]
+                        state = "innum"
+                    elif input_text[i] in comp:
+                        temp_token += input_text[i]
+                        state = "incomp"
+                    elif input_text[i] == ':':
+                        temp_token += input_text[i]
+                        state = "inassign"
+                    elif input_text[i] == "=":
+                        temp_token += input_text[i]
+                        state = "doneincr"
+                    elif input_text[i] in special_tokens:
+                        temp_token += input_text[i]
+                        state = "doneincr"
 
-
-def submit():
-    targetRegEx = text_edit.toPlainText()
-
-    i = 0  # initial counter that iterates over the targetRegEx string
-
-    while i < len(targetRegEx):
-
-        temp_string = ''
-
-        if targetRegEx[i].isdigit():  # NUM
-            temp_string += targetRegEx[i]
-            i += 1
-            while i < len(targetRegEx):
-                if targetRegEx[i].isdigit():
-                    temp_string += targetRegEx[i]
-
-                elif targetRegEx[i].isalpha():
-                    print('Wrong Token !')
-                    exit()
-                i += 1
-            lexemes.append(temp_string)
-
-        elif targetRegEx[i].isalpha():  # ID # TODO (_)
-            temp_string += targetRegEx[i]
-            i += 1
-            while i < len(targetRegEx) and (targetRegEx[i].isalpha() or targetRegEx[i].isdigit()):
-                temp_string += targetRegEx[i]
-                i += 1
-            lexemes.append(temp_string)
-
-        elif targetRegEx[i] in tokens:  # (';', '=')
-            lexemes.append(targetRegEx[i])
-            i += 1
-
-        elif targetRegEx[i] == '>' or targetRegEx[i] == '<':  # ('>', '<' '>=', '<=')
-            temp_string += targetRegEx[i]
-            i += 1
-            if i < len(targetRegEx) and targetRegEx[i] == '=':
-                temp_string += targetRegEx[i]
-                i += 1
-            lexemes.append(temp_string)
-
-        elif targetRegEx[i] == ':':  # (':=')
-            temp_string += targetRegEx[i]
-            i += 1
-            if i < len(targetRegEx) and targetRegEx[i] == '=':
-                temp_string += targetRegEx[i]
-                i += 1
+                elif state == "inid":
+                    if input_text[i].isdigit() or input_text[i].isalpha():
+                        temp_token += input_text[i]
+                        state = "inid"
+                    else:
+                        state = "done"
+                elif state == "innum":
+                    if input_text[i].isdigit():
+                        temp_token += input_text[i]
+                        state = "innum"
+                    elif input_text[i].isalpha():
+                        state = "error"
+                    else:
+                        state = "done"
+                elif state == "inassign":
+                    if input_text[i] == "=":
+                        temp_token += input_text[i]
+                        state = "doneincr"
+                    else:
+                        state = "error"
+                elif state == "incomp":
+                    if input_text[i] == "=":
+                        temp_token += input_text[i]
+                        state = "doneincr"
+                    else:
+                        state = "done"
             else:
-                print('Wrong Token !')
-                exit()
-            lexemes.append(temp_string)
+                state = "error"
+            if state == "error":
+                print("syntax error")
+                break
+            if state != "done":
+                i += 1
+            if state == "done" or state == "doneincr":
+                tokens.append(temp_token)
+                temp_token = ""
+                # i-=1
+                state = "start"
+        if (state == "inassign"):
+            print("syntax error")
+        elif (state != "start" and state != "doneincr"):
+            tokens.append(temp_token)
+        status = "Accepted"
+        if state == "error":
+            status = "Rejected"
 
-        elif targetRegEx[i] == ' ' or targetRegEx[i] == '\t' or targetRegEx[i] == '\n' or targetRegEx[i] == '\0':
-            i += 1
-
-        else:
-            print('Wrong Expression !')
-            exit()
-
-    for i in lexemes:
-        if i.isdigit():
-            types.append('NUM')
-        elif i in comparators:
-            types.append('comparators')
-        elif i in reserved_words:
-            types.append(i)
-        elif i in assignment:
-            types.append('assignment')
-        elif i == ';':
-            types.append(';')
-        else:
-            types.append('ID')
-
-    print('lexemes list:', lexemes, '\n')
-    print('types list:', types, '\n')
-    exit()
-
-
-app = QApplication([])
-main_widget = QWidget()
-
-text_edit = QTextEdit()
-label = QLabel('Enter the TINY sample code below !')
-submit_button = QPushButton('Submit')
-submit_button.clicked.connect(submit)
-
-grid = QGridLayout()
-grid.addWidget(label)
-grid.addWidget(text_edit)
-grid.addWidget(submit_button)
-
-main_widget.setGeometry(300, 300, 400, 400)
-main_widget.setWindowTitle('Lexer')
-main_widget.setLayout(grid)
-main_widget.show()
-
-app.exec()
-
-# targetRegEx = 'repeatxah>=m>s<as<=e;d=5:=5'
+        for i in tokens:
+            if i in reserved_words:
+                types.append(i)
+            elif i in comparison_operators:
+                types.append("COMP")
+            elif i == ";":
+                types.append(";")
+            elif i.isdigit():
+                types.append("NUM")
+            elif i == ":=":
+                types.append("assign")
+            else:
+                types.append("ID")
+        return (status,tokens, types)
