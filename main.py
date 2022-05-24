155c2cd4246dbe5974f25a980a1e0ba370875ff3
TeraByte256 <tbyte256@gmail.com>
2022-05-24 14:49:45 +0200

    Added the stack table and stack table integration in the main window

--- main.py
+++ main.py
@@ -1,50 +1,60 @@
-from PyQt5.QtWidgets import QApplication, QPushButton, QGridLayout, QLabel, QTextEdit, QWidget, QTableWidget, QTableWidgetItem,QHBoxLayout,QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QPushButton, QLabel, QTextEdit, QWidget, QTableWidget, \
+    QTableWidgetItem, QHBoxLayout, QVBoxLayout
 from grammar import Grammar
 from scanner import Scanner
 from dfa import dfa
 from graph import Graph
+from stack_table import StackTable
+
 sc = Scanner()
 gr = Grammar()
 dfa = dfa()
 dfaresult = []
+
+
 def submit():
-    global  dfaresult
+    global dfaresult
     input = text_edit.toPlainText()
     result = sc.scan(input)
     status = result[0]
     tokens = result[1]
     types = result[2]
     gr.func(types)
-    scanner_status_label.setText("Scanner status: "+status)
-
+    scanner_status_label.setText("Scanner status: " + status)
 
     j = 0
-    while (j < len(tokens)):
+    while j < len(tokens):
         print('token:', tokens[j], ",type:", types[j])
         j += 1
-    if(status=='Accepted'):
+    if status == 'Accepted':
         dfa_button.setEnabled(True)
         dfaresult = dfa.scan(types)
         syntax_status_label.setText("Syntax status: " + dfaresult[0])
     else:
         dfa_button.setEnabled(False)
-        types[len(types)-1]="illegal token"  #modify this
+        types[len(types) - 1] = "illegal token"  # modify this
     populateTable(tokens, types)
+
 
 def showdfa():
     global dfaresult
     Graph(dfaresult[1])
 
-def populateTable(tokens,types):
+
+def populateTable(tokens, types):
     row = 0
-    while(row<len(tokens)):
+    while row < len(tokens):
         table.setRowCount(len(tokens))
-        table.setItem(row,0,QTableWidgetItem(tokens[row]))
+        table.setItem(row, 0, QTableWidgetItem(tokens[row]))
         table.setItem(row, 1, QTableWidgetItem(types[row]))
-        row+=1
+        row += 1
 
-    #table.insertRow(currentrowcount,0,QTableWidgetItem("Some text"))
+    # table.insertRow(currentrowcount,0,QTableWidgetItem("Some text"))
 
+
+def show_stack_table():
+    dialog = StackTable(parse_list=gr.stack_table)
+    dialog.exec_()
 
 
 app = QApplication([])
@@ -57,34 +67,36 @@ scanner_status_label = QLabel("Scanner status: Waiting...")
 syntax_status_label = QLabel("Syntax status: Waiting...")
 submit_button = QPushButton('Submit')
 dfa_button = QPushButton('Show DFA')
+stack_button = QPushButton('Show Stack Table')
 submit_button.clicked.connect(submit)
 dfa_button.setEnabled(False)
 dfa_button.clicked.connect(showdfa)
+stack_button.clicked.connect(show_stack_table)
 
-table= QTableWidget()
+table = QTableWidget()
 table.setColumnCount(2)
 
-table.setHorizontalHeaderLabels(["Token","Type"])
+table.setHorizontalHeaderLabels(["Token", "Type"])
 table.horizontalHeader().setStretchLastSection(True)
 
-
-#grid = QGridLayout()
+# grid = QGridLayout()
 grid = QVBoxLayout()
 grid.addWidget(label)
 subgrid = QHBoxLayout()
 subgrid.addWidget(text_edit)
 subgrid.addWidget(table)
-#grid.addWidget(text_edit)
+# grid.addWidget(text_edit)
 grid.addLayout((subgrid))
 grid.addWidget(scanner_status_label)
 grid.addWidget(syntax_status_label)
 grid.addWidget(submit_button)
 grid.addWidget(dfa_button)
-#grid.addWidget(tableWidget)
+grid.addWidget(stack_button)
+# grid.addWidget(tableWidget)
 
 
-#main_widget.setGeometry(500, 500, 900, 700)
-main_widget.setFixedSize(900,500)
+# main_widget.setGeometry(500, 500, 900, 700)
+main_widget.setFixedSize(900, 500)
 
 main_widget.setWindowTitle('Lexer')
 main_widget.setLayout(grid)
